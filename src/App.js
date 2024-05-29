f42d1462f95032558083872c128cc6340bb71466
SomyaMishraDev <mishrasomya551@gmail.com>
2024-05-30 00:12:40 +0530

    Shubham Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import LoginSignup from './components/LoginSignup/LoginSignup';
 import './App.css';
 
 function App() {
   return (
     <div>
-     <LoginSignup/>
+     <LoginSignup />
     </div>
   );
 }
